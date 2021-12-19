224d0a912d054d5fb794213d5474dbc1f57465aa
hailita1 <hailit2306@gmail.com>
2021-12-19 11:33:57 +0700

    first commit

--- src/models/checklist.js
+++ src/models/checklist.js
@@ -1,24 +1,24 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Checklist extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Checklist.init({
-    content: DataTypes.STRING,
-    result: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Checklist',
-  });
-  return Checklist;
+    class Checklist extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    Checklist.init({
+        content: DataTypes.STRING,
+        result: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Checklist',
+    });
+    return Checklist;
 };
